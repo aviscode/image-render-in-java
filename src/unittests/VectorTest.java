d07aabeea01a7684b13fe2242745ed52906f3556
avrumi rosenberg <avrumi96@gmail.com>
2020-03-24 23:26:03 +0200

    1.final

---
+++ src/unittests/VectorTest.java
@@ -0,0 +1,78 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VectorTest extends Object {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void scale() {
+    }
+
+    @Test
+    public void dotProduct() {
+    }
+
+    @Test
+    public void crossProduct() {
+    }
+
+    @Test
+    public void lengthSquared() {
+    }
+
+    @Test
+    public void length() {
+    }
+
+    @Test
+    public void normalize() {
+    }
+
+    @Test
+    public void normalized() {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testSubtract() {
+    }
+
+    @Test
+    public void testScale() {
+    }
+
+    @Test
+    public void testDotProduct() {
+    }
+
+    @Test
+    public void testCrossProduct() {
+    }
+
+    @Test
+    public void testLengthSquared() {
+    }
+
+    @Test
+    public void testLength() {
+    }
+
+    @Test
+    public void testNormalize() {
+    }
+
+    @Test
+    public void testNormalized() {
+    }
+}
