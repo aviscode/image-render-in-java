bd612b9474a8ddf0233b401cc0e06daa2cd57d86
mendylep <57071984+mendylep@users.noreply.github.com>
2020-05-08 16:47:54 +0300

    1.6

--- src/elements/SpotLight.java
+++ src/elements/SpotLight.java
@@ -3,10 +3,21 @@ package elements;
 import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector;
-
+/**
+ * The type Spot light.
+ */
 public class SpotLight extends PointLight {
     private Vector _direction;
-
+    /**
+     * Instantiates a new Spot light.
+     *
+     * @param direction   the directio
+     * @param intensity   the intensit
+     * @param kC          the kC
+     * @param kL          the kL
+     * @param kQ          the  kQ
+     * @param position    the  position
+     */
     public SpotLight(Color intensity, Point3D position, Vector direction, double kC, double kL, double kQ) {
         super(intensity, position, kC, kL, kQ);
         _direction = new Vector(direction).normalized();
